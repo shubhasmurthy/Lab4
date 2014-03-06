4182e8de7c3ecfd2930894d3e1164bbe85dc5a11
Tanmaya <tanela16@gmail.com>
2014-03-06 12:05:30 -0700

    add as favourite

--- Lab4Trial/src/edu/asupoly/ser422/lab4/controller/HandleAll.java
+++ Lab4Trial/src/edu/asupoly/ser422/lab4/controller/HandleAll.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,51 +26,59 @@ public class HandleAll extends HttpServlet {
 		// TODO Auto-generated constructor stub
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		handleIt(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-			IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		handleIt(request, response);
 	}
 
-	private void handleIt(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-			IOException {
+	private void handleIt(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
 		PrintWriter out = response.getWriter();
 		if (action == null) {
 			populateNews(request, null);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("goToLogin")) {
-			RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("login.jsp");
 			dispatcher.forward(request, response);
 		} else if (action.equals("viewNews") || action.equals("Cancel")) {
 			HttpSession session = request.getSession(false);
 			UserBean user = (UserBean) session.getAttribute("user");
 			populateNews(request, user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("Login")) {
 			String userId = request.getParameter("userid");
 			String passwd = request.getParameter("passwd");
 			String role = request.getParameter("role");
 			if (role != null) {
-				if (userId == null || userId.length() == 0 || passwd == null || passwd.length() == 0) {
+				if (userId == null || userId.length() == 0 || passwd == null
+						|| passwd.length() == 0) {
 					response.sendRedirect("login.jsp?role=Subscriber");
 				} else if (!userId.equals(passwd)) {
 					response.sendRedirect("login.jsp?role=Subscriber");
 				} else {
 					HttpSession session = request.getSession(true);
-					UserBean user = BizLogicHandler.createNewSubscriber(userId, passwd);
+					UserBean user = BizLogicHandler.createNewSubscriber(userId,
+							passwd);
 					session.setAttribute("user", user);
 					populateNews(request, user);
-					RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+					RequestDispatcher dispatcher = request
+							.getRequestDispatcher(BizLogicHandler.getUrl());
 					dispatcher.forward(request, response);
 				}
 			} else {
-				if (userId == null || userId.length() == 0 || passwd == null || passwd.length() == 0) {
+				if (userId == null || userId.length() == 0 || passwd == null
+						|| passwd.length() == 0) {
 					response.sendRedirect("login.jsp");
 				} else if (!userId.equals(passwd)) {
 					response.sendRedirect("login.jsp");
@@ -82,17 +91,21 @@ public class HandleAll extends HttpServlet {
 					} else {
 
 					}
-					RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+					RequestDispatcher dispatcher = request
+							.getRequestDispatcher(BizLogicHandler.getUrl());
 					dispatcher.forward(request, response);
 				}
 			}
 		} else if (action.equals("about")) {
-			RequestDispatcher dispatcher = request.getRequestDispatcher("about.jsp");
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("about.jsp");
 			dispatcher.forward(request, response);
 		} else if (action.equals("ViewStory")) {
-			NewsItemBean nib = BizLogicHandler.getStoryItem(Integer.parseInt(request.getParameter("newsid")));
+			NewsItemBean nib = BizLogicHandler.getStoryItem(Integer
+					.parseInt(request.getParameter("newsid")));
 			request.setAttribute("newsItem", nib);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("A new subscriber")) {
 			String userId = request.getParameter("userid");
@@ -101,7 +114,8 @@ public class HandleAll extends HttpServlet {
 			UserBean user = BizLogicHandler.createNewSubscriber(userId, passwd);
 			session.setAttribute("user", user);
 			populateNews(request, user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("A new reporter")) {
 			String userId = request.getParameter("userid");
@@ -110,70 +124,178 @@ public class HandleAll extends HttpServlet {
 			UserBean user = BizLogicHandler.createNewReporter(userId, passwd);
 			session.setAttribute("user", user);
 			populateNews(request, user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("logout")) {
 			HttpSession session = request.getSession(false);
 			session.invalidate();
 			response.sendRedirect(request.getContextPath());
 		} else if (action.equals("Add as favorite")) {
-			int newsID = Integer.parseInt(request.getParameter("newsid"));
-			NewsItemBean nib = NewsDAOFactory.getTheDAO().getNewsItem(newsID);
-			NewsDAOFactory.getTheDAO().storeFavorite(newsID);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("ViewNews.jsp");
-			dispatcher.forward(request, response);
+			// int newsID = Integer.parseInt(request.getParameter("newsid"));
+			boolean setCookie = false;
+			String userId = request.getParameter("userid");
+			String passwd = request.getParameter("passwd");
+			String role = request.getParameter("role");
+
+			if (userId == null || role.contains("Guest")) {
+				String newsId = request.getParameter("newsid");
+				Cookie[] cookie = request.getCookies();
+				if (cookie == null)// no favorites yet
+				{
+					Cookie newCookie = new Cookie("newsid", newsId);
+					newCookie.setMaxAge(86400);
+					response.addCookie(newCookie);
+					response.sendRedirect(request.getContextPath());
+
+				} else {
+					int i = 0;
+					for (i = 0; i < cookie.length; i++) {
+						if (cookie[i].getName().contains("newsid")) {
+							String oldCookie = cookie[i].getValue();
+							newsId = oldCookie + "\t" + newsId;
+							cookie[i].setValue(newsId);
+							cookie[i].setMaxAge(86400);
+							setCookie = true;
+							break;
+						}
+					}
+
+					if ((i == cookie.length) && (setCookie == false)) {
+						Cookie newCookie = new Cookie("newsid", newsId);
+						newCookie.setMaxAge(86400);
+						response.addCookie(newCookie);
+						response.sendRedirect(request.getContextPath());
+					}
+				}
+			} else if (role.contains("SUBSCRIBER")) {
+				String newsId = request.getParameter("newsid");
+				String subCookie = "sub" + userId + "newsid";
+				Cookie[] cookie = request.getCookies();
+				if (cookie == null)// no favorites yet
+				{
+					Cookie newCookie = new Cookie(subCookie, newsId);
+					response.addCookie(newCookie);
+					response.sendRedirect(request.getContextPath());
+
+				} else {
+					int i = 0;
+					for (i = 0; i < cookie.length; i++) {
+						if (cookie[i].getName().contains(subCookie)) {
+							String oldCookie = cookie[i].getValue();
+							newsId = oldCookie + "\t" + newsId;
+							cookie[i].setValue(newsId);
+							cookie[i].setMaxAge(86400);
+							setCookie = true;
+							break;
+						}
+					}
+
+					if ((i == cookie.length) && (setCookie == false)) {
+						Cookie newCookie = new Cookie(subCookie, newsId);
+						newCookie.setMaxAge(86400);
+						response.addCookie(newCookie);
+						response.sendRedirect(request.getContextPath());
+					}
+				}
+			} else if (role.contains("REPORTER")) {
+				String newsId = request.getParameter("newsid");
+				String repCookie = "rep" + userId + "newsid";
+				Cookie[] cookie = request.getCookies();
+				if (cookie == null)// no favorites yet
+				{
+					Cookie newCookie = new Cookie(repCookie, newsId);
+					response.addCookie(newCookie);
+					response.sendRedirect(request.getContextPath());
+
+				} else {
+					int i = 0;
+					for (i = 0; i < cookie.length; i++) {
+						if (cookie[i].getName().contains(repCookie)) {
+							String oldCookie = cookie[i].getValue();
+							newsId = oldCookie + "\t" + newsId;
+							break;
+						}
+					}
+
+					if ((i == cookie.length) && (setCookie == false)) {
+						Cookie newCookie = new Cookie(repCookie, newsId);
+						newCookie.setMaxAge(86400);
+						response.addCookie(newCookie);
+						response.sendRedirect(request.getContextPath());
+					}
+				}
+			}
 		} else if (action.equals("Remove as favorite")) {
-			int newsID = Integer.parseInt(request.getParameter("newsid"));
-			NewsItemBean nib = NewsDAOFactory.getTheDAO().getNewsItem(newsID);
-			NewsDAOFactory.getTheDAO().removeFavorite(newsID);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("ViewNews.jsp");
-			dispatcher.forward(request, response);
+			String userId = request.getParameter("userid");
+			String passwd = request.getParameter("passwd");
+			String role = request.getParameter("role");
+			
+			if(role==null || role.contains("GUEST"))
+			{
+				
+			}
+
 		} else if (action.equals("create")) {
-			RequestDispatcher dispatcher = request.getRequestDispatcher("CreateNewStory.jsp");
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("CreateNewStory.jsp");
 			dispatcher.forward(request, response);
 		} else if (action.equals("Add")) {
-			UserBean user = (UserBean) request.getSession().getAttribute("user");
+			UserBean user = (UserBean) request.getSession()
+					.getAttribute("user");
 			String title = request.getParameter("title");
 			String story = request.getParameter("story");
-			boolean isPublic = request.getParameter("isPublic").equals("subscribersOnly") ? false : true;
+			boolean isPublic = request.getParameter("isPublic").equals(
+					"subscribersOnly") ? false : true;
 			BizLogicHandler.addStory(title, story, user.getUserId(), isPublic);
 			populateNews(request, user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("Update")) {
 			out.println("Here");
-			UserBean user = (UserBean) request.getSession().getAttribute("user");
+			UserBean user = (UserBean) request.getSession()
+					.getAttribute("user");
 			String title = request.getParameter("title");
 			String story = request.getParameter("story");
 			int newsID = Integer.parseInt(request.getParameter("newsid"));
-			boolean isPublic = request.getParameter("isPublic").equals("subscribersOnly") ? false : true;
+			boolean isPublic = request.getParameter("isPublic").equals(
+					"subscribersOnly") ? false : true;
 			BizLogicHandler.updateStory(newsID, title, story, isPublic);
 			populateNews(request, user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("delete")) {
 			int newsID = Integer.parseInt(request.getParameter("newsid"));
-			UserBean user = (UserBean) request.getSession().getAttribute("user");
+			UserBean user = (UserBean) request.getSession()
+					.getAttribute("user");
 			BizLogicHandler.removeStory(newsID);
 			populateNews(request, user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		} else if (action.equals("edit")) {
 			int newsID = Integer.parseInt(request.getParameter("newsid"));
-			UserBean userid = (UserBean) request.getSession().getAttribute("user");
+			UserBean userid = (UserBean) request.getSession().getAttribute(
+					"user");
 			NewsItemBean nib = BizLogicHandler.getStoryItem(newsID);
 			request.setAttribute("newsItem", nib);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("CreateNewStory.jsp");
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("CreateNewStory.jsp");
 			dispatcher.forward(request, response);
 		} else if (action.equals("comment")) {
 			int newsID = Integer.parseInt(request.getParameter("newsid"));
-			UserBean user = (UserBean) request.getSession().getAttribute("user");
+			UserBean user = (UserBean) request.getSession()
+					.getAttribute("user");
 			String comment = request.getParameter("Comment");
 			BizLogicHandler.storeComment(newsID, user.getUserId(), comment);
 			populateNews(request, user);
-			RequestDispatcher dispatcher = request.getRequestDispatcher(BizLogicHandler.getUrl());
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(BizLogicHandler.getUrl());
 			dispatcher.forward(request, response);
 		}
+
 	}
 
 	private void populateNews(HttpServletRequest request, UserBean user) {
